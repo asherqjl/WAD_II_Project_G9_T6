a60f81fe653a09e92f9cdf8256c4d116a36a5925
asherqjl <quekjiale@gmail.com>
2021-11-02 17:58:52 +0800

    registers

--- js/login-registration.js
+++ js/login-registration.js
@@ -7,44 +7,42 @@ const app = Vue.createApp({
             loginPwd:'',
             loginEmail:''
         };
+    },
+    methods: {
+        register() {
+            const url = 'http://localhost:8888/WAD_II_Project_G9_T6/db/register.php'
+            const data = { userEmail: this.userEmail,
+                            cfmPwd: this.cfmPwd,
+                            showStatus: false,
+                            status: ''
+                        }
+            // POST request
+            /* axios.post(url, data
+            )
+            .then(response => {
+                console.log(response.data)
+                this.showStatus = true
+                this.status = response.data
+            })
+            .catch(error => {
+                this.showStatus = true
+                this.status = 'There was an error: ' + error.message 
+            })  */
+            // GET request
+            axios.get(url, {
+                params: data
+            })
+            .then(response => {
+                console.log(response.data)
+                this.showStatus = true
+                this.status = response.data
+            })
+            .catch(error => {
+                this.showStatus = true
+                this.status = 'There was an error: ' + error.message 
+            }) 
+            
+        }
     }
-});
-// DO NOT EDIT - start
-app.component('include-navbar',{
-    template: ` 
-        <div><nav class="navbar navbar-expand-md">
-            <div class="container-fluid">
-                <a href="home.html" class="navbar-brand">SmilingAcrossLocal<span style="font-size: 11px;"> SG</span></a> 
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                        <span></span>
-                        <span></span>
-                        <span></span>
-                    </button>
-                <div class="collapse navbar-collapse justify-content-between" id="navbarNav">
-                    <ul class="navbar-nav ms-auto">
-                        <li class="nav-item active"> 
-                            <a class="nav-link" aria-current="page" href="login.html"> LOGIN/SIGNUP </a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="home.html">HOME</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="KaiWei.html">ATTRACTIONS</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="DongHyun.html">MAPS</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="zenyu.html">TRAVEL HISTORY</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="FangTing.html">REWARDS</a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </nav></div>
-    `    
-});
+}).mount('#signup')
 
-const vm = app.mount("#navdiv");
