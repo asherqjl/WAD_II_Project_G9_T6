13234b8b03280bef95c7d33f57a3b983f8b4f72c
asherqjl <quekjiale@gmail.com>
2021-11-08 20:57:04 +0800

    session change nav bar

--- js/navbar.js
+++ js/navbar.js
@@ -8,15 +8,23 @@ app.component('include-navbar',{
         <div><nav class="navbar navbar-expand-md">
             <div class="container-fluid">
                 <a href="home.html" class="navbar-brand">SmilingAcrossLocal<span style="font-size: 11px;"> SG</span></a> 
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" 
+                                   data-bs-target="#navbarNav" aria-controls="navbarNav"
+                                   aria-expanded="false" aria-label="Toggle navigation">
                         <span></span>
                         <span></span>
                         <span></span>
                     </button>
                 <div class="collapse navbar-collapse justify-content-between" id="navbarNav">
                     <ul class="navbar-nav ms-auto">
-                        <li class="nav-item active"> 
-                            <a class="nav-link" aria-current="page" href="login-signup.html"> LOGIN/SIGNUP </a>
+                        <li id="profile" class="nav-item active"> 
+                            <a id="UserName" class="nav-link" aria-current="page" href="profileSettings.html"></a>
+                        </li>
+                        <li id="signout" class="nav-item active"> 
+                            <a class="nav-link" aria-current="page" href="signout.html">Sign Out</a>
+                        </li>
+                        <li id="LoginSignUp" class="nav-item active"> 
+                            <a class="nav-link" aria-current="page" href="login-signup.html">LOGIN/SIGNUP</a>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link" href="home.html">HOME</a>
--- js/sessions.js
+++ js/sessions.js
@@ -1,21 +1,25 @@
+const displayUserName = document.querySelector('#UserName');
+const profileTab = document.querySelector('#profile');
+const signoutTab = document.querySelector('#signout');
+const loginSignupTab = document.querySelector('#LoginSignUp');
+
 // define the nameDisplayCheck() function
 function nameDisplayCheck() {
     // check whether the 'name' data item is stored in web Storage
-    if(localStorage.getItem('name')) {
-        // If it is, display personalized greeting
-        let name = localStorage.getItem('name');
-        h1.textContent = 'Welcome, ' + name;
-        personalGreeting.textContent = 'Welcome to our website, ' + name + '! We hope you have fun while you are here.';
-        // hide the 'remember' part of the form and show the 'forget' part
-        forgetDiv.style.display = 'block';
-        rememberDiv.style.display = 'none';
+    if(localStorage.getItem('user_name')) {
+        // If it is, display name
+        let name = localStorage.getItem('user_name');
+        displayUserName.textContent =  name;
+        profileTab.style.display = "block"
+        signoutTab.style.display = "block"
+        loginSignupTab.style.display = "none"
+
     } else {
-        // if not, display generic greeting
-        h1.textContent = 'Welcome to our website ';
-        personalGreeting.textContent = 'Welcome to our website. We hope you have fun while you are here.';
-        // hide the 'forget' part of the form and show the 'remember' part
-        forgetDiv.style.display = 'none';
-        rememberDiv.style.display = 'block';
+        // if not just login/sign up
+        loginSignupTab.style.display = "block"
+        signoutTab.style.display = "none"
+        profileTab.style.display = "none"
+
     }
 }
 
