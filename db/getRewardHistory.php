0cf85afe11f17ddb23a420183a4c878e7c28c304
zenyu1 <ongzenyu112@gmail.com>
2021-11-13 16:49:42 +0800

    aaa

--- db/getRewardHistory.php
+++ db/getRewardHistory.php
@@ -1,7 +1,7 @@
 <?php
 require_once 'common.php';
 $dao = new PostDAO();
-$posts = $dao->getAllRewards(); // Get an Indexed Array of Post objects
+$posts = $dao->getAllRewards($email); // Get an Indexed Array of Post objects
 
 $items = [];
 foreach( $posts as $post_object ) {
