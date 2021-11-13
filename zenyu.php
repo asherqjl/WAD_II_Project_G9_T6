266bca9cfffb2812bee049645e1d258a68a71280
zenyu1 <ongzenyu112@gmail.com>
2021-11-13 23:19:49 +0800

    dwada

--- zenyu.php
+++ zenyu.php
@@ -23,31 +23,7 @@
         </div>
     
         <div id="app3">
-            <div class='row'>
-                <div class='col-sm d-flex justify-content-center' >
-                    <img  v-if="travel_history.length===0" src='./images/sad.png' class='img-fluid d-flex justify-content-center '></img>
-                    <img  v-else src='./images/happy.png' class='img-fluid d-flex justify-content-center'></img>
-                </div>
-            </div>
 
-            <div class='row'>
-                <div class='col-sm d-flex justify-content-center'>
-                    <h1 v-if="travel_history.length===0">No History</h1>
-                    <h1 v-else> Your Travel History</h1>
-                </div>
-            </div>
-
-            <div class='row'>
-                <div class='col'>
-                </div>
-                <div class='col text-center'>
-                    <form action="./DongHyun.html">
-                        <input type="submit" class="btn btn-secondary" value="New Journey" />
-                    </form>
-                </div>
-                <div class='col'>
-                </div>
-            </div>
 
             <!-- <div class="d-flex justify-content-center "> -->
                 <!-- <table class="table bg-white w-75"> -->
@@ -57,36 +33,57 @@
                     <!-- </tr> -->
                 <!-- </table> -->
             <!-- </div> -->
+        <div id="map" class="map-responsive"></div>
 
-            <div class="row" >
-                <div class="d-flex justify-content-center" >
-                    <table class="table bg-white w-75" id="listingTable">
-                    </table> 
-                </div>
-            
-                
-                <button v-on:click="prevPage" id="btn_prev">Prev</button>
-                <button v-on:click="nextPage" id="btn_next">Next</button>
-                page: <span id="page"></span>  
-            </div>   
-        </div>
     </body>
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDT6sTbyN3xQA9bTHQYcyFVXsqAXT54zfE&callback=initMap"
+        async defer></script>
     <script type="text/javascript" src="js/navbar.js"></script>
     <script type="text/javascript" src="js/sessions.js"></script>
     <script type="text/javascript" src="js/signOutPopUp.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
     <script>
+        var infowindow;
+        function initMap() {
+        const map = new google.maps.Map(document.getElementById("map"), {
+        
+                zoom: 11.7,
+                center: { lat: 1.3649170000000002, lng: 103.82287200000002 },
+                styles: [    
+        {
+            featureType: "poi",
+            elementType: "labels",
+            stylers: [{ visibility: "off" }]
+        }
+    ]
+            });
+            setMarkers(map);
+            
+  
+        
+            
 
-        const app3 = Vue.createApp({
-            data(){
-                return {
-                    travel_history:'',
-                    current_page: 1,
-                    records_per_page: 3,
 
-                }
-            },
-            mounted:function()  {
+
+
+            }
+        
+            function setMarkers(map)  {
+                const image = {
+                    url: "https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png",
+                    // This marker is 20 pixels wide by 32 pixels high.
+                    size: new google.maps.Size(30, 48),
+                    scaledSize: new google.maps.Size(30, 48),
+                    // The origin for this image is (0, 0).
+                    origin: new google.maps.Point(0, 0),
+                    // The anchor for this image is the base of the flagpole at (0, 32).
+                    anchor: new google.maps.Point(0, 32),
+                };
+                const shape = {
+                    coords: [1, 1, 1, 20, 18, 20, 18, 1],
+                    type: "poly",
+                };
+                var travel_history=''
                     // POST request
                     /* axios.post(url, data
                     )
@@ -108,67 +105,45 @@
             })
                     .then(response => { 
                         console.log(response.data)
-                        this.travel_history= response.data.reverse()
-                        console.log(this.travel_history)
-                        this.status = response.data
-                        this.changePage(1)
+                        var lat_lng_list=response.data;
+                        for (let i=0;i<lat_lng_list.length;i++){
+                            const attraction=lat_lng_list[i];
+
+                            var marker=new google.maps.Marker({
+                                position:{lat:parseFloat(attraction.latitude),lng:parseFloat(attraction.longitude)},
+                                map,
+                                icon:image,
+                                shape:shape,
+                                title:"visited place"
+                            })
+
+                            var content='<div><h3>'+attraction.time_visited+'</h3></div>'
+                            infowindow=new google.maps.InfoWindow()
+                            google.maps.event.addListener(marker,'click', (function(marker,content,infowindow){ 
+                            return function() {
+                                alert("wad");
+                                infowindow.setContent(content);
+                                infowindow.open(map,marker);
+                                };
+                            })(marker,content,infowindow)); 
+                        }
+                        
+
+
+
+
+
+
+                        
                     })
                     .catch(error => {
-                        this.status = 'There was an error: ' + error.message 
+                        status = 'There was an error: ' + error.message 
                     }) 
                     
-                },
-                methods:{
-                    prevPage(){
-                        if (this.current_page > 1) {
-                            this.current_page--
-                            this.changePage(this.current_page)
-                        }
-                    },
-                    nextPage(){
-                        if(this.current_page < this.numPages()){ 
-                            console.log(this.numPages())
-                            this.current_page++
-                            this.changePage(this.current_page)
-                        }
-                    },
-                    changePage(page){
-                        var btn_next = document.getElementById("btn_next")
-                        var btn_prev = document.getElementById("btn_prev")
-                        var listing_table = document.getElementById("listingTable")
-                        var page_span = document.getElementById("page")
-
-                        // Validate page
-                        if (page < 1) page = 1
-                        if (page > this.numPages()) page = this.numPages()  
-
-                        listing_table.innerHTML = ""
-                        for (var i = (page-1) * this.records_per_page; i < (page * this.records_per_page); i++) {
-                            if(i<this.travel_history.length){
-                            listing_table.innerHTML+= `<tr><td><h2>${this.travel_history[i].location_name}</h2><br/>${this.travel_history[i].category}</td>
-                                <td>${this.travel_history[i].time_visited}</td></tr>`                                    
-                            }
-                        }
-
-                        page_span.innerHTML = page
-                        if (page == 1) {
-                            btn_prev.style.visibility = "hidden"
-                        } else {
-                            btn_prev.style.visibility = "visible"
-                        }
-
-                        if (page == this.numPages()) {
-                            btn_next.style.visibility = "hidden"
-                        } else {
-                            btn_next.style.visibility = "visible"
-                        }
-                    },
-                    numPages(){
-                        return Math.ceil(this.travel_history.length / this.records_per_page )
-                    }       
                 }
-        })
-        const vm3 = app3.mount("#app3");
 
+                
+               
+    
     </script>
 </html>
